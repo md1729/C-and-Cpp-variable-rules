d53afdde80632040ef95c6e3390b20eb2d96b917
md1729 <muhammet.dilek@metu.edu.tr>
2024-02-10 21:02:49 +0300

    variable rules of c++
    We explained name of variable rules for this project and we coded c and cpp programming language

---
+++ Variable rules of name.cpp
@@ -0,0 +1,21 @@
+//Variable rules of name:
+/*
+1.does not use Turkish character
+2.does not start with numbers
+3.does not gap between the words.
+4."_" can be used as a special character.
+5.If it is emerged from  more than one word,it can be used Camel Case.(nameSurname)
+6.IF it is emerged from more than one word,it can be used snake method(ex.:name_surname)
+*/
+#include<iostream>
+using namespace std;
+main(){
+	setlocale(LC_ALL,"English");
+	int number1;
+	int number2=10;
+	cout<<number2<<endl;
+	number1=15;
+	cout<< number1<<endl;
+	cout<<" your first number " << number1<<" your second number " << number2<<endl;
+	
+}
---
+++ variables.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main(){
+ int number1=10,number3,number4;
+ int number2;	
+ number2=20;
+ printf("%d and %d\n",number1,number2);
+ printf("Hello");	
+}
